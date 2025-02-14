a6144bf9757baa268747183d7020c5d361966dde
selvazvin <selvazvin@gmail.com>
2025-02-14 11:09:06 +0400

    updated form date script

--- script.js
+++ script.js
@@ -14,12 +14,39 @@ submit.addEventListener("click", function () {
     var limitamountRegex = /^[0-9]+$/
     var BenefnameRegex = /^[a-z A-Z]+$/
 
+    var d1Regex = /^[0-3]/
+    var d2Regex = /^[0-9]/
+    var m1Regex = /^[0-1]/
+    var m2Regex = /^[0-2]/
+    var y1Regex = /^[0-2]/
+    var y2Regex = /^[0-9]/
+    var y3Regex = /^[0-9]/
+    var y4Regex = /^[0-9]/
+
     var donoraccno = document.getElementById("donoraccno")
     var donortitle = document.getElementById("donortitle")
     var recipaccno = document.getElementById("recipaccno")
     var reciptitle = document.getElementById("reciptitle")
     var limitamount = document.getElementById("limitamount")
     var Benefname = document.getElementById("Benefname")
+
+    var sd1 = document.getElementById("sd1")
+    var sd2 = document.getElementById("sd2")
+    var sm1 = document.getElementById("sm1")
+    var sm2 = document.getElementById("sm2")
+    var sy1 = document.getElementById("sy1")
+    var sy2 = document.getElementById("sy2")
+    var sy3 = document.getElementById("sy3")
+    var sy4 = document.getElementById("sy4")
+
+    var ed1 = document.getElementById("ed1")
+    var ed2 = document.getElementById("ed2")
+    var em1 = document.getElementById("em1")
+    var em2 = document.getElementById("em2")
+    var ey1 = document.getElementById("ey1")
+    var ey2 = document.getElementById("ey2")
+    var ey3 = document.getElementById("ey3")
+    var ey4 = document.getElementById("ey4")
 
     var validate = true
 
@@ -52,27 +79,149 @@ submit.addEventListener("click", function () {
     else {
         document.querySelector(".reciptitleerror").style.display = "none"
     }
-    if(limitamountRegex.test(limitamount.value)==false)
-    {
-        document.querySelector(".limitamounterror").style.display="inline"
+    if (limitamountRegex.test(limitamount.value) == false) {
+        document.querySelector(".limitamounterror").style.display = "inline"
         validate = false
     }
-    else
-    {
-        document.querySelector(".limitamounterror").style.display="none"
+    else {
+        document.querySelector(".limitamounterror").style.display = "none"
     }
-    if(BenefnameRegex.test(Benefname.value)==false)
-    {
-        document.querySelector(".Benefnameerror").style.display="inline"
-        validate=false
+    if (BenefnameRegex.test(Benefname.value) == false) {
+        document.querySelector(".Benefnameerror").style.display = "inline"
+        validate = false
     }
-    else
-    {
-        document.querySelector(".Benefnameerror").style.display="none"
+    else {
+        document.querySelector(".Benefnameerror").style.display = "none"
     }
-    if(validate==true)
-        {
-            alert("Form submission successfully")
-        }
-    
+
+    if (d1Regex.test(sd1.value) == false) {
+        document.getElementById("sd1").style.borderColor = "red"
+        validate = false
+    }
+    else {
+        document.getElementById("sd1").style.borderColor = "grey"
+    }
+    if (d2Regex.test(sd2.value) == false) {
+        document.getElementById("sd2").style.borderColor = "red"
+        validate = false
+    }
+    else {
+        document.getElementById("sd2").style.borderColor = "grey"
+    }
+
+    if (m1Regex.test(sm1.value) == false) {
+        document.getElementById("sm1").style.borderColor = "red"
+        validate = false
+    }
+    else {
+        document.getElementById("sm1").style.borderColor = "grey"
+    }
+
+    if (m2Regex.test(sm2.value) == false) {
+        document.getElementById("sm2").style.borderColor = "red"
+        validate = false
+    }
+    else {
+        document.getElementById("sm2").style.borderColor = "grey"
+    }
+    if (y1Regex.test(sy1.value) == false) {
+        document.getElementById("sy1").style.borderColor = "red"
+        validate = false
+    }
+    else {
+        document.getElementById("sy1").style.borderColor = "grey"
+    }
+
+    if (y2Regex.test(sy2.value) == false) {
+        document.getElementById("sy2").style.borderColor = "red"
+        validate = false
+    }
+    else {
+        document.getElementById("sy2").style.borderColor = "grey"
+    }
+    if (y3Regex.test(sy3.value) == false) {
+        document.getElementById("sy3").style.borderColor = "red"
+        validate = false
+    }
+    else {
+        document.getElementById("sy3").style.borderColor = "grey"
+    }
+    if (y4Regex.test(sy4.value) == false) {
+        document.getElementById("sy4").style.borderColor = "red"
+        validate = false
+    }
+    else {
+        document.getElementById("sy4").style.borderColor = "grey"
+    }
+
+
+// end date
+if (d1Regex.test(ed1.value) == false) {
+    document.getElementById("ed1").style.borderColor = "red"
+    validate = false
+}
+else {
+    document.getElementById("ed1").style.borderColor = "grey"
+}
+if (d2Regex.test(ed2.value) == false) {
+    document.getElementById("ed2").style.borderColor = "red"
+    validate = false
+}
+else {
+    document.getElementById("ed2").style.borderColor = "grey"
+}
+
+if (m1Regex.test(em1.value) == false) {
+    document.getElementById("em1").style.borderColor = "red"
+    validate = false
+}
+else {
+    document.getElementById("em1").style.borderColor = "grey"
+}
+
+if (m2Regex.test(em2.value) == false) {
+    document.getElementById("em2").style.borderColor = "red"
+    validate = false
+}
+else {
+    document.getElementById("em2").style.borderColor = "grey"
+}
+if (y1Regex.test(ey1.value) == false) {
+    document.getElementById("ey1").style.borderColor = "red"
+    validate = false
+}
+else {
+    document.getElementById("ey1").style.borderColor = "grey"
+}
+
+if (y2Regex.test(ey2.value) == false) {
+    document.getElementById("ey2").style.borderColor = "red"
+    validate = false
+}
+else {
+    document.getElementById("ey2").style.borderColor = "grey"
+}
+if (y3Regex.test(ey3.value) == false) {
+    document.getElementById("ey3").style.borderColor = "red"
+    validate = false
+}
+else {
+    document.getElementById("ey3").style.borderColor = "grey"
+}
+if (y4Regex.test(ey4.value) == false) {
+    document.getElementById("ey4").style.borderColor = "red"
+    validate = false
+}
+else {
+    document.getElementById("ey4").style.borderColor = "grey"
+}
+
+
+
+
+
+    if (validate == true) {
+        alert("Form submission successfully")
+    }
+
 })
